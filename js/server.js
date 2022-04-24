6c173134a231257beced19ee976e872d24a4a22a
Smart-UI web development agency <92440177+Company-Smart-ui@users.noreply.github.com>
2022-04-24 20:09:24 +0300

    'autopush'

--- js/server.js
+++ js/server.js
@@ -22,7 +22,7 @@ const Utils = require("./utils.js");
  * @class
  */
 function Server(config, callback) {
-	const port = process.env.MM_PORT || config.port;
+	const port = process.env.PORT || config.port;
 
 	let server = null;
 	if (config.useHttps) {
